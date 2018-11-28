e2c48963418fba43f879b58106821c31d4c514b0
Brandon Haskett <brhaskett@yahoo.com>
2018-11-27 21:52:07 -0500

    added going to gig button

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,40 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var currentUser = User.Identity.GetUserId();
+            var exists = _context.Attendances.Any(x => x.AttendeeId == currentUser && x.GigId == dto.GigId);
+
+            if(exists)
+            {
+                return BadRequest("Already attending");
+            }
+
+            var attendance = new Attendance()
+            {
+                GigId = dto.GigId,
+                AttendeeId = currentUser
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
